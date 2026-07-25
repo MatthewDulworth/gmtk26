4d6f6ab913756afa04849c3c80f0e2850f24f79b
Matthew Dulworth <mhdulworth@gmail.com>
2026-07-24 22:38:38 -0500

    Gun works basic

--- components/HurtBox.gd
+++ components/HurtBox.gd
@@ -11,3 +11,4 @@ signal hit(amount: float, source: Node)
 func take_damage(amount: float, source: Node = null) -> void:
 	health.take_damage(amount)
 	hit.emit(amount, source)
+	print(amount, source)

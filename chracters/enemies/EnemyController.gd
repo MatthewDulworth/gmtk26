d85e342551b88e68ee67b9b4c6ae57b971b7796f
PeterDulworth <pdulworth97@gmail.com>
2026-07-24 19:02:23 -0700

    nav agent

--- chracters/enemies/EnemyController.gd
+++ chracters/enemies/EnemyController.gd
@@ -5,6 +5,10 @@ class_name EnemyController
 @export var health: Health
 @export var hurtbox: HurtBox
 
+@onready var nav_agent_2d = $NavigationAgent2D
+
+var target: Vector2
+
 const player_scan_time = 0.3 # Time in seconds until it scans for which player to chase
 
 enum EnemyState {
@@ -19,7 +23,9 @@ func _ready() -> void:
 	#health.initialize(enemy_data.max_health)
 
 func _process(_delta: float) -> void:
-	$AnimatedSprite2D.play("idle")
+	$AnimatedSprite2D.play("flap")
+	#if (position.distance_to(target)) > 0.5:
+		
 
 func _physics_process(_delta: float) ->  void:
 	pass

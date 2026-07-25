35373bcc1183d3632f214c32fca5bbc1743d93a8
Dulworth, Luke <44180963+LukeDul@users.noreply.github.com>
2026-07-24 21:28:32 -0500

    first pass on spawner

---
+++ lukes_level_DONT_TOUCH/enemy_spawner.gd
@@ -0,0 +1,42 @@
+extends Marker2D
+
+@export var enemy_scene: PackedScene
+
+var _time_since_last_spawn = 0
+var _time_since_wave_start = 0
+
+var enemy_count:
+	set(v):
+		if v == 0:
+			current_wave += 1
+		enemy_count = v
+
+var current_spawn_rate: float = 1.0
+var current_wave_length: float = 10.0
+var current_wave = 0: 
+	set(v):
+		current_wave_length *= 1.1
+		current_spawn_rate *= 0.9
+		_time_since_wave_start = 0
+		current_wave = v
+	get:
+		return current_wave
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	enemy_count = 0
+
+func _process(delta: float) -> void:
+	if _time_since_wave_start > current_wave_length:
+		return
+	
+	if _time_since_last_spawn > current_spawn_rate:
+		var new_enemy = enemy_scene.instantiate()
+		add_child(new_enemy)
+		enemy_count += 1
+		_time_since_last_spawn = 0
+
+	_time_since_last_spawn += delta
+	_time_since_wave_start += delta
+	

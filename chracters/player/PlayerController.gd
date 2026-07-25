f2b9242104356a0923c4e9b6e67a7235e3e3328b
PeterDulworth <pdulworth97@gmail.com>
2026-07-24 21:30:52 -0700

    Revert "Merge branch 'master' of https://github.com/MatthewDulworth/gmtk26"
    This reverts commit 763d278296183cad57ecb0cb99f2e11a28e6f420, reversing
    changes made to af89f2058220abf297ea5e7d4e6357a11f35d745.

--- chracters/player/PlayerController.gd
+++ chracters/player/PlayerController.gd
@@ -4,31 +4,18 @@ class_name PlayerController
 @export var player_data: PlayerData
 @export var health: Health
 @export var hurtbox: HurtBox
-@export var weapon: Weapon
 
+var current_weapon: Weapon
 var player_id: InputManager.PlayerID = InputManager.PlayerID.PLAYER_1
 var input: InputIntent
 
 func _ready() -> void:
 	health.initialize(player_data.max_health)
 	input = InputManager.get_input_intent(player_id)
-	health.died.connect(_on_death)
-	_use_weapon(player_data.starting_weapon)
-	$AnimatedSprite2D.play("levitate")
 
 func _process(_delta: float) -> void:
 	input = InputManager.get_input_intent(player_id)
-	
-	if (input.fire_just_pressed):
-		print(input.facing)
-		weapon.fire(input.facing)
-
-func _use_weapon(data: WeaponData) -> void:
-	weapon.initialize(data)
 
 func _physics_process(_delta: float) -> void:
 	velocity = input.move * player_data.move_speed
 	move_and_slide()
-
-func _on_death() -> void:
-	print(player_id, " died")
--- components/Projectile.gd
+++ components/Projectile.gd
@@ -4,40 +4,24 @@ class_name Projectile
 var damage: float
 var source: Node
 var velocity: Vector2
-var lifetime: float
-var _time_alive: float
 
 func _ready() -> void:
+	monitoring = true
 	area_entered.connect(_on_area_entered)
 
-func activate(_damage: float, _source: Node, radius: float, dir: Vector2, speed: float, spawn_position: Vector2, _lifetime: float) -> void:
+func initialize(_damage: float, _source: Node, radius: float, _velocity: Vector2) -> void:
 	damage = _damage
 	source = _source
-	velocity = dir * speed
-	lifetime = _lifetime
-	_time_alive = 0.0
-	global_position = spawn_position
 	var shape := CircleShape2D.new()
 	shape.radius = radius
 	$CollisionShape2D.shape = shape
-	monitoring = true
-	visible = true
-	set_physics_process(true)
 
 func _physics_process(delta: float) -> void:
 	position += velocity * delta
-	_time_alive += delta
-	if _time_alive >= lifetime:
-		_deactivate()
 
 func _on_area_entered(area: Area2D) -> void:
 	if not (area is HurtBox):
 		return
 	area.take_damage(damage, self)
-	_deactivate()
-
-func _deactivate() -> void:
 	monitoring = false
-	visible = false
-	set_physics_process(false)
-	ProjectilePool.release(self)
+	queue_free()

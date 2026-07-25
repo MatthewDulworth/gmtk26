f071e2d05be72fcd15ec9e1ce0917e406975c1f7
Matthew Dulworth <mhdulworth@gmail.com>
2026-07-24 20:45:55 -0500

    it lives

--- chracters/player/PlayerController.gd
+++ chracters/player/PlayerController.gd
@@ -5,11 +5,17 @@ class_name PlayerController
 @export var health: Health
 @export var hurtbox: HurtBox
 
+var current_weapon: Weapon
+var player_id: InputManager.PlayerID = InputManager.PlayerID.PLAYER_1
+var input: InputIntent
+
 func _ready() -> void:
 	health.initialize(player_data.max_health)
+	input = InputManager.get_input_intent(player_id)
 
 func _process(_delta: float) -> void:
-	pass
+	input = InputManager.get_input_intent(player_id)
 
 func _physics_process(_delta: float) -> void:
-	pass
+	velocity = input.move * player_data.move_speed
+	move_and_slide()

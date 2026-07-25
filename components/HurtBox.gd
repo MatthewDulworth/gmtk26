01dfe05e8aa2615c36c74077294d73fb37a2796e
Matthew Dulworth <mhdulworth@gmail.com>
2026-07-24 19:47:26 -0500

    stuff, updated to 4.7.1

--- components/HurtBox.gd
+++ components/HurtBox.gd
@@ -1,12 +1,13 @@
 extends Area2D
 class_name HurtBox
 
-@onready var health: Health = $"../Health"
+@export var health: Health
+
 var max_invuln_time: float
 var invuln_time: float
 
-signal hit
+signal hit(amount: float, source: Node)
 
 func take_damage(amount: float, source: Node = null) -> void:
 	health.take_damage(amount)
-	hit.emit()
+	hit.emit(amount, source)
--- components/Projectile.gd
+++ components/Projectile.gd
@@ -1,4 +1,27 @@
 extends Area2D
 class_name Projectile
 
-var damage
+var damage: float
+var source: Node
+var velocity: Vector2
+
+func _ready() -> void:
+	monitoring = true
+	area_entered.connect(_on_area_entered)
+
+func initialize(_damage: float, _source: Node, radius: float, _velocity: Vector2) -> void:
+	damage = _damage
+	source = _source
+	var shape := CircleShape2D.new()
+	shape.radius = radius
+	$CollisionShape2D.shape = shape
+
+func _physics_process(delta: float) -> void:
+	position += velocity * delta
+
+func _on_area_entered(area: Area2D) -> void:
+	if not (area is HurtBox):
+		return
+	area.take_damage(damage, self)
+	monitoring = false
+	queue_free()
